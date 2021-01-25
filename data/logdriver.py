ca7f03d35ae98bae7445241bf4db91b5bef0d2a9
vcokltfre <vcokltfre@gmail.com>
2021-01-25 15:19:21 +0000

    feat: add logs endpoint

---
+++ data/logdriver.py
@@ -0,0 +1,32 @@
+from aioredis import create_redis_pool
+from randutils import formatstring
+from asyncio import get_event_loop
+
+from fastapi.exceptions import HTTPException
+from starlette.responses import PlainTextResponse
+
+
+class LogDriver:
+    def __init__(self):
+        self.pool = None
+
+    async def init(self):
+        if not self.pool:
+            self.pool = await create_redis_pool("redis://redis")
+
+    async def create_log(self, text: str) -> str:
+        await self.init()
+        _id = formatstring("xxxxxx-xxxxxxxxxxxx-xxxxxx")
+
+        await self.pool.set(_id, text, expire=86400 * 14) # Logs expire after 14 days
+
+        return _id
+
+    async def get_log(self, logid: str):
+        await self.init()
+        log = await self.pool.get(logid)
+
+        if not log:
+            raise HTTPException(404, "Log not found.")
+
+        return PlainTextResponse(log)
--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+from data.models import LogCreate
+from data.logdriver import LogDriver
+from src.response import ok
+from src.auth.auth import authenticate
 
 app = FastAPI(docs_url=None)
+logs = LogDriver()
 
 @app.get('/')
 async def get_root():
-    return {"status":"ok"}
+    return ok()
+
+@app.post("/logs")
+async def create_log(data: LogCreate, request: Request):
+    authenticate(request)
+
+    _id = await logs.create_log(data.text)
+
+    return ok(id=_id)
+
+@app.get("/logs/{logid}")
+async def get_log(logid: str):
+    return await logs.get_log(logid)
